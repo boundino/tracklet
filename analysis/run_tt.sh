b1106b21ef653ba949942915d658e246ec3ba075
boundino <boundino@gmail.com>
2022-11-21 12:14:51 +0100

    up run.sh

--- analysis/run_tt.sh
+++ analysis/run_tt.sh
@@ -1,5 +1,5 @@
 make transmute_trees || exit 1
-./transmute_trees ../../pixelsim_presplitting.root out.root
+./transmute_trees ../../pixeldata_presplitting.root outdata.root
 # sem --wait --id sem-grp-tt-def
 # ls out.root.* > notes/tt-merge-out-2022-11-03-23_22_52
 # nice -n 10 sem -j-4 --id sem-grp-tt-def --linebuffer ./transmute_trees ../../pixelsim_presplitting.root out.root.0 0 1024 \ &> notes/log-tt-out-10-0.log
