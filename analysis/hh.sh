935538c961fee72b829a81aaa00ea5460bec20a1
boundino <boundino@gmail.com>
2023-01-30 21:27:28 +0100

    up hh.sh structure

--- analysis/hh.sh
+++ analysis/hh.sh
@@ -2,35 +2,90 @@
 
 make harvest_hists || exit 1
 
-RUN=362294
+RUN_PIX_2D=0
+RUN_TRK_2D=0
+RUN_PIX_1D=1 ; TAG_PIX=362294t-step
+RUN_TRK_1D=1 ; TAG_TRACKLET=362294t-step
 
-INPUT_PIX_DATA=/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/pixelpre_221201_HITestRaw0_HIRun2022A_MBPVfilTh4_${RUN}.root
-INPUT_PIX_HYDJET=/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/pixelpre_221229_Hydjet_Drum5F_PbPb_5360GeV_221224_GTv7priZ0_Th4.root
-INPUT_PIX_EPOS=/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/pixelpre_221229_EposLHC_ReggeGribovParton_PbPb_5360GeV_221224_GTv7priZ0_Th4.root
-INPUT_PIX_AMPTST=/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/pixelpre_221229_AMPT_StringMelting_PbPb_5360GeV_221224_GTv7priZ0_Th4.root
-INPUT_PIX_AMPTNO=/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/pixelpre_221229_AMPT_NoStringMelting_PbPb_5360GeV_221224_GTv7priZ0_Th4.root
+INPUTS_PIX=(
+    "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/pixel_230126_HITestRaw0_HIRun2022A_MBPVfilTh4_362294.root,362294,362294t"
+    "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/pixelpre_221201_HITestRaw0_HIRun2022A_MBPVfilTh4_362294.root,362294 (Presplit),362294t-presplit"
+    "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/pixel_230129_Hydjet_Drum5F_PbPb_5360GeV_230129_GTv8priZ0_GTv8Th4.root,HYDJET,Hydjet"
+    "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/pixel_230126_Hydjet_Drum5F_PbPb_5360GeV_221224_GTv7priZ0_GTv8Th4.root,HYDJET (Old bad),Hydjet-v7"
+    "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/pixelpre_221229_Hydjet_Drum5F_PbPb_5360GeV_221224_GTv7priZ0_Th4.root,HYDJET (Presplit),Hydjet-presplit"
+    # "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/pixelpre_221229_EposLHC_ReggeGribovParton_PbPb_5360GeV_221224_GTv7priZ0_Th4.root,EPOS LHC,Epos"
+    # "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/pixelpre_221229_AMPT_StringMelting_PbPb_5360GeV_221224_GTv7priZ0_Th4.root,Hydjet,AMPT (w. String Melt),AmptWsm"
+    # "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/pixelpre_221229_AMPT_NoStringMelting_PbPb_5360GeV_221224_GTv7priZ0_Th4.root,AMPT (w/o String Melt),AmptNsm"
+)
 
-INPUT_TRACKLET_DATA=/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/tt_221218_pixelpre_221201_HITestRaw0-6_HIRun2022A_MBPVfilTh4_${RUN}.root
-INPUT_TRACKLET_HYDJET=/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/tt_221229_vzshift_pixelpre_221229_Hydjet_Drum5F_PbPb_5360GeV_221224_GTv7priZ0_Th4.root
-INPUT_TRACKLET_EPOS=/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/tt_221229_vzshift_pixelpre_221229_EposLHC_ReggeGribovParton_PbPb_5360GeV_221224_GTv7priZ0_Th4.root
-INPUT_TRACKLET_AMPTST=/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/tt_221229_vzshift_pixelpre_221229_AMPT_StringMelting_PbPb_5360GeV_221224_GTv7priZ0_Th4.root
-INPUT_TRACKLET_AMPTNO=/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/tt_221229_vzshift_pixelpre_221229_AMPT_NoStringMelting_PbPb_5360GeV_221224_GTv7priZ0_Th4.root
+INPUTS_TRACKLET=(
+    "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/tt_230127_pixel_230126_HITestRaw0-5_HIRun2022A_MBPVfilTh4_362294.root,362294,362294t"
+    "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/tt_221218_pixelpre_221201_HITestRaw0-6_HIRun2022A_MBPVfilTh4_362294.root,362294 (Presplit),362294t-presplit"
+    "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/tt_230130_pixel_230129_Hydjet_Drum5F_PbPb_5360GeV_230129_GTv8priZ0_GTv8Th4.root,HYDJET,Hydjet"
+    "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/tt_230127_pixel_230126_Hydjet_Drum5F_PbPb_5360GeV_221224_GTv7priZ0_GTv8Th4.root,HYDJET (Old bad),Hydjet-v7"
+    # "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/tt_230129_bkg5_pixel_230126_Hydjet_Drum5F_PbPb_5360GeV_221224_GTv7priZ0_GTv8Th4.root,HYDJET (5% Background),Hydjet-bkg5"
+    "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/tt_221229_vzshift_pixelpre_221229_Hydjet_Drum5F_PbPb_5360GeV_221224_GTv7priZ0_Th4.root,HYDJET (Presplit),Hydjet-presplit"
+    # "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/tt_221229_vzshift_pixelpre_221229_EposLHC_ReggeGribovParton_PbPb_5360GeV_221224_GTv7priZ0_Th4.root,EPOS LHC,Epos"
+    # "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/tt_221229_vzshift_pixelpre_221229_AMPT_StringMelting_PbPb_5360GeV_221224_GTv7priZ0_Th4.root,AMPT (w. String Melt),AmptWsm"
+    # "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/tt_221229_vzshift_pixelpre_221229_AMPT_NoStringMelting_PbPb_5360GeV_221224_GTv7priZ0_Th4.root,AMPT (w/o String Melt),AmptNsm"
+)
+
+ARG_PIX_INPUT=
+ARG_PIX_LEG=
+for i in "${INPUTS_PIX[@]}"
+do
+    IFS="," ; inputs=($i) ; unset IFS ;
+    ARG_PIX_INPUT=${ARG_PIX_INPUT}" "${inputs[0]}
+    ARG_PIX_LEG=${ARG_PIX_LEG}"${inputs[1]},"
+done
+ARG_PIX_LEG=${ARG_PIX_LEG%%,}
+
+ARG_TRACKLET_INPUT=
+ARG_TRACKLET_LEG=
+for i in "${INPUTS_TRACKLET[@]}"
+do
+    IFS="," ; inputs=($i) ; unset IFS ;
+    ARG_TRACKLET_INPUT=${ARG_TRACKLET_INPUT}" "${inputs[0]}
+    ARG_TRACKLET_LEG=${ARG_TRACKLET_LEG}"${inputs[1]},"
+done
+ARG_TRACKLET_LEG=${ARG_TRACKLET_LEG%%,}
 
 echo 'token
-std::vector<std::string> files = '$INPUT_PIX_DATA' '$INPUT_PIX_HYDJET' '$INPUT_PIX_EPOS' '$INPUT_PIX_AMPTST' '$INPUT_PIX_AMPTNO'
+std::vector<std::string> files = '$ARG_PIX_INPUT'
 token ,
-std::vector<std::string> legends = Run '${RUN}',HYDJET,EPOS,AMPT (String Melt),AMPT (No String Melt)
-' > lists/pixel_data_${RUN}.list
+std::vector<std::string> legends = '$ARG_PIX_LEG'
+' > lists/pixel.list
 
 echo 'token
-std::vector<std::string> files = '$INPUT_TRACKLET_DATA' '$INPUT_TRACKLET_HYDJET' '$INPUT_TRACKLET_EPOS' '$INPUT_TRACKLET_AMPTST' '$INPUT_TRACKLET_AMPTNO'
+std::vector<std::string> files = '$ARG_TRACKLET_INPUT'
 token ,
-std::vector<std::string> legends = Run '${RUN}',HYDJET,EPOS,AMPT (String Melt),AMPT (No String Melt)
-' > lists/tracklet_data_${RUN}.list
+std::vector<std::string> legends = '$ARG_TRACKLET_LEG'
+' > lists/tracklet.list
 
-./harvest_hists 0 lists/pixel_data_${RUN}.list ${RUN}t 
-./harvest_hists 1 lists/tracklet_data_${RUN}.list ${RUN}t 
-./harvest_hists 2 $INPUT_PIX_DATA ${RUN}t 
-./harvest_hists 2 $INPUT_PIX_HYDJET Hydjet 
-./harvest_hists 3 $INPUT_TRACKLET_DATA ${RUN}t 
+[[ $RUN_PIX_2D -eq 1 ]] && {
+    ## Pixel 2D
+    for i in "${INPUTS_PIX[@]}"
+    do
+        IFS="," ; inputs=($i) ; unset IFS ;
+        ./harvest_hists 2 ${inputs[0]} "${inputs[2]}"
+    done
+}
 
+[[ $RUN_TRK_2D -eq 1 ]] && {
+    ## Tracklet 2D
+    for i in "${INPUTS_TRACKLET[@]}"
+    do
+        IFS="," ; inputs=($i) ; unset IFS ;
+        ./harvest_hists 3 ${inputs[0]} "${inputs[2]}"
+    done
+}
+
+## Pixel 1D
+[[ $RUN_PIX_1D -eq 1 ]] && {
+    ./harvest_hists 0 lists/pixel.list $TAG_PIX
+}
+
+## Tracklet 1D
+[[ $RUN_TRK_1D -eq 1 ]] && {
+    ./harvest_hists 1 lists/tracklet.list $TAG_TRACKLET
+}
