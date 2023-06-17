75af88544999bd6c61e53b59a5fcaa13fda41bb9
boundino <boundino@gmail.com>
2023-06-18 00:01:43 +0200

    up legend labels

--- analysis/include/tool.h
+++ analysis/include/tool.h
@@ -3,10 +3,20 @@
 
 #include "xjjcuti.h"
 
+static const int goodrange[] = { 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 };
+void goodresult(TH1* h, const int* goodbin = goodrange) {
+  for(int i=0; i<h->GetXaxis()->GetNbins(); i++) {
+    if(goodbin[i] == 0) {
+      h->SetBinContent(i+1, 0);
+      h->SetBinError(i+1, 0);
+    }
+  }
+}
+
 std::string tcomb(std::string comb) {
   std::string r(comb);
   r.insert(1, " #otimes ");
-  r = Form("%sPIX %s", (atoi(comb.c_str())>50?"F":"B"), r.c_str());
+  r = Form("%s#scale[0.8]{PIX} %s", (atoi(comb.c_str())>50?"F":"B"), r.c_str());
   return r;
 }
 
@@ -15,7 +25,7 @@ std::string tcent(std::string tag) {
   if(!xjjc::str_contains(tag, ".s.")) return "";
   tag = xjjc::str_erasestar(tag, "*.s.");
   auto v = xjjc::str_divide(tag, ".");
-  if(v.size() != 2) return "";
+  if(v.size() < 2) return "";
   float cmax = 5*(20-atof(v[0].c_str())),
     cmin = 5*(20-atof(v[1].c_str()));
   return Form("Cent. %.0f-%.0f%s", cmin, cmax, "%");
