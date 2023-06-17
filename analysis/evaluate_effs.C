0db4e7d53811fb18959ed5f807cb8d4201389349
boundino <boundino@gmail.com>
2023-06-18 00:01:12 +0200

    add evaluate_eff

---
+++ analysis/evaluate_effs.C
@@ -0,0 +1,98 @@
+#include "TFile.h"
+#include "TTree.h"
+#include "TProfile.h"
+#include "TH2D.h"
+#include "TColor.h"
+#include "TCanvas.h"
+#include "TLegend.h"
+
+#include <vector>
+#include <string>
+
+#include "git/config/configurer.h"
+
+#include "include/cosmetics.h"
+#include "include/xjjmypdf.h"
+
+int evaluate_effs(const char* config, const char* label) {
+  configurer* conf = new configurer(config);
+
+  auto files = conf->get<std::vector<std::string>>("files");
+  auto legends = conf->get<std::vector<std::string>>("legends");
+
+  std::size_t nfiles = files.size();
+  if (!nfiles) { printf("error: no files provided!\n"); return 1; }
+
+  TFile* foutput = new TFile(Form("output/vertex-%s.root", label), "recreate");
+
+  const int nresbins = 8;
+  const double resbins[nresbins + 1] = {
+    0, 20, 40, 60, 100, 150, 200, 300, 500};
+
+  TFile* f[nfiles]; TTree* t[nfiles];
+  TProfile* heff[nfiles]; TH2D* hvznhit1[nfiles]; TH1D* hres[nfiles];
+  for (std::size_t i = 0; i < nfiles; ++i) {
+    f[i] = new TFile(files[i].data(), "read");
+    t[i] = (TTree*)f[i]->Get("TrackletTree12");
+
+    foutput->cd();
+
+    heff[i] = new TProfile(Form("heff_%zu", i), "", 20, 0, 100);
+    t[i]->Draw(Form("vz[1]>-98:nhit1>>heff_%zu", i),
+               "ntracklet>0 && abs(vz[0])<15", "goff", 16384);
+    hstyle(heff[i], markers[i % ncolours], colours[i % ncolours]);
+
+    hvznhit1[i] = new TH2D(Form("hvznhit1_%zu", i), "",
+                           nresbins, resbins, 100, -0.5, 0.5);
+    t[i]->Draw(Form("vz[1]-vz[0]:nhit1>>hvznhit1_%zu", i),
+               "ntracklet>0 && abs(vz[0])<15", "goff colz", 16384);
+    hvznhit1[i]->FitSlicesY();
+    hres[i] = (TH1D*)gDirectory->Get(Form("hvznhit1_%zu_2", i));
+    hstyle(hres[i], markers[i % ncolours], colours[i % ncolours]);
+  }
+
+  xjjroot::setgstyle(1);
+  gStyle->SetPadLeftMargin(xjjroot::margin_pad_left/1.5);
+  gStyle->SetPadBottomMargin(xjjroot::margin_pad_bottom/1.2);
+  xjjroot::mypdf pdf(Form("figspdf/vertex/vertex-%s.pdf", label), "c", 600, 600);
+   
+  pdf.prepare();
+  TLegend* l1 = new TLegend(0.5, 0.56, 0.9, 0.72);
+  for (std::size_t i = 0; i < nfiles; ++i) {
+    hrange(heff[i], 0, 1.2);
+    htitle(heff[i], ";number of pixel hits (layer 1);efficiency");
+    heff[i]->Draw("p e same");
+    l1->AddEntry(heff[i], legends[i].data(), "pl");
+  }
+  lstyle(l1, 43, 16); l1->Draw();
+  pdf.write(Form("figs/vertex/vertex-eff-%s.png", label));
+
+  pdf.prepare();
+  pdf.getc()->SetLogy();
+  TLegend* l2 = new TLegend(0.5, 0.56, 0.9, 0.72);
+  for (std::size_t i = 0; i < nfiles; ++i) {
+    hrange(hres[i], 0.001, 0.5);
+    htitle(hres[i], ";number of pixel hits (layer 1);resolution (cm)");
+    if(!i) hres[i]->Draw("axis");
+    hres[i]->Draw("p e same");
+    l2->AddEntry(hres[i], legends[i].data(), "pl");
+  }
+  lstyle(l2, 43, 16); l2->Draw();
+  pdf.write(Form("figs/vertex/vertex-res-%s.png", label));
+
+  pdf.close();
+
+  foutput->Write("", TObject::kOverwrite);
+  foutput->Close();
+
+  return 0;
+}
+
+int main(int argc, char* argv[]) {
+  if (argc == 3) {
+    return evaluate_effs(argv[1], argv[2]);
+  } else {
+    printf("usage: ./evaluate_effs [config] [label]\n");
+    return 1;
+  }
+}
--- analysis/hh.sh
+++ analysis/hh.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
 make harvest_hists || exit 1
+make evaluate_effs || exit 1
 
 RUN_PIX_2D=1
 RUN_TRK_2D=1
 RUN_PIX_1D=1 ; TAG_PIX=362294a
 RUN_TRK_1D=1 ; TAG_TRACKLET=362294a
+RUN_VTX_EFF=1 ; TAG_VERTEX=harvest
+
+. tool.shinc 
 
 INPUTS_PIX=(
     "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/pixel_230126_HITestRaw0_HIRun2022A_MBPVfilTh4_362294.root,362294,362294a"
-    "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/pixel_230129_EposLHC_ReggeGribovParton_PbPb_5360GeV_230129_GTv8priZ0_GTv8Th4.root,EPOS LHC,epos"
-    "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/pixel_230129_Hydjet_Drum5F_PbPb_5360GeV_230129_GTv8priZ0_GTv8Th4.root,HYDJET,hydjet"
-    "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/pixel_230129_AMPT_StringMelting_PbPb_5360GeV_230129_GTv8priZ0_GTv8Th4.root,AMPT (w. String Melt),amptsm"
+    "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/pixel_230129_EposLHC_ReggeGribovParton_PbPb_5360GeV_230129_GTv8priZ0_GTv8Th4.root,${taglabel[epos]},epos"
+    "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/pixel_230129_Hydjet_Drum5F_PbPb_5360GeV_230129_GTv8priZ0_GTv8Th4.root,${taglabel[hydjet]},hydjet"
+    "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/pixel_230129_AMPT_StringMelting_PbPb_5360GeV_230129_GTv8priZ0_GTv8Th4.root,${taglabel[amptsm]},amptsm"
+    "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/vpresplit/pixelpre_221229_AMPT_NoStringMelting_PbPb_5360GeV_221224_GTv7priZ0_Th4.root,${taglabel[amptnm]},amptnm"
 )
 
 INPUTS_TRACKLET=(
-    "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/tt_230127_pixel_230126_HITestRaw0-5_HIRun2022A_MBPVfilTh4_362294.root,362294,362294a"
-    "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/tt_230130_pixel_230129_EposLHC_ReggeGribovParton_PbPb_5360GeV_230129_GTv8priZ0_GTv8Th4.root,EPOS LHC,epos"
-    "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/tt_230130_pixel_230129_Hydjet_Drum5F_PbPb_5360GeV_230129_GTv8priZ0_GTv8Th4.root,HYDJET,hydjet"
-    "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/tt_230130_pixel_230129_AMPT_StringMelting_PbPb_5360GeV_230129_GTv8priZ0_GTv8Th4.root,AMPT (w. String Melt),amptsm"
+    "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/tt_230322_pixel_230126_HITestRaw0-6_HIRun2022A_MBPVfilTh4_362294.root,362294,362294a"
+    "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/tt_230322_pixel_230129_EposLHC_ReggeGribovParton_PbPb_5360GeV_230129_GTv8priZ0_GTv8Th4.root,${taglabel[epos]},epos"
+    "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/tt_230322_pixel_230129_Hydjet_Drum5F_PbPb_5360GeV_230129_GTv8priZ0_GTv8Th4.root,${taglabel[hydjet]},hydjet"
+    "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/tt_230322_pixel_230129_AMPT_StringMelting_PbPb_5360GeV_230129_GTv8priZ0_GTv8Th4.root,${taglabel[amptsm]},amptsm"
+    "/eos/cms/store/group/phys_heavyions/wangj/tracklet2022/vpresplit/tt_221229_vzshift_pixelpre_221229_AMPT_NoStringMelting_PbPb_5360GeV_221224_GTv7priZ0_Th4.root,${taglabel[amptnm]},amptnm"
 )
 
 ARG_PIX_INPUT=
@@ -41,17 +47,34 @@ do
 done
 ARG_TRACKLET_LEG=${ARG_TRACKLET_LEG%%,}
 
+ARG_VERTEX_INPUT=
+ARG_VERTEX_LEG=
+for i in "${INPUTS_TRACKLET[@]}"
+do
+    IFS="," ; inputs=($i) ; unset IFS ;
+    [[ ${inputs[2]} == *a ]] && continue
+    ARG_VERTEX_INPUT=${ARG_VERTEX_INPUT}" "${inputs[0]}
+    ARG_VERTEX_LEG=${ARG_VERTEX_LEG}"${inputs[1]},"
+done
+ARG_VERTEX_LEG=${ARG_VERTEX_LEG%%,}
+
 echo 'token
-std::vector<std::string> files = '$ARG_PIX_INPUT'
+std::vector<std::string> files ='$ARG_PIX_INPUT'
 token ,
-std::vector<std::string> legends = '$ARG_PIX_LEG'
+std::vector<std::string> legends ='$ARG_PIX_LEG'
 ' > lists/pixel.list
 
 echo 'token
-std::vector<std::string> files = '$ARG_TRACKLET_INPUT'
+std::vector<std::string> files ='$ARG_TRACKLET_INPUT'
 token ,
-std::vector<std::string> legends = '$ARG_TRACKLET_LEG'
+std::vector<std::string> legends ='$ARG_TRACKLET_LEG'
 ' > lists/tracklet.list
+
+echo 'token
+std::vector<std::string> files ='$ARG_VERTEX_INPUT'
+token ,
+std::vector<std::string> legends ='$ARG_VERTEX_LEG'
+' > lists/vertex.list
 
 [[ $RUN_PIX_2D -eq 1 ]] && {
     ## Pixel 2D
@@ -79,5 +102,10 @@ std::vector<std::string> legends = '$ARG_TRACKLET_LEG'
 ## Tracklet 1D
 [[ $RUN_TRK_1D -eq 1 ]] && {
     ./harvest_hists 1 lists/tracklet.list $TAG_TRACKLET
+}
+
+## Vertex eff
+[[ $RUN_VTX_EFF -eq 1 ]] && {
+    ./evaluate_effs lists/vertex.list $TAG_VERTEX
 }
 
