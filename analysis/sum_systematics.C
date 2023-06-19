5af8fb60d9101607fd43fa9b46542184e3325343
boundino <boundino@gmail.com>
2023-06-19 19:49:18 +0200

    add clean drawing in sum_syst

--- analysis/sum_systematics.C
+++ analysis/sum_systematics.C
@@ -23,10 +23,10 @@ int macro(std::string input_avg, std::string tag,
 
   std::vector<Color_t> cc = xjjroot::colorlist_light;
   TH1F* hsym = xjjroot::gethist<TH1F>(iavg.value[0][0]+".root::hsym");
-  auto legPIX = new TLegend(0.3, 0.47-0.031*9, 0.3+0.2, 0.47);
+  auto legPIX = new TLegend(0.3, 0.45-0.030*9, 0.3+0.2, 0.45);
   xjjroot::setleg(legPIX, 0.028);
   auto h1WEfinal = combh1WEfinal(iavg.value[0][0]+".root", legPIX);
-  auto legTRUTH = new TLegend(0.55, 0.47-0.031*4, 0.55+0.2, 0.47);
+  auto legTRUTH = new TLegend(0.55, 0.45-0.030*5, 0.55+0.2, 0.45-0.030);
   xjjroot::setleg(legTRUTH, 0.028);
   auto gh1WGhadron = combgh1WGhadron(iavg.value[0][0]+".root", legTRUTH);
 
@@ -50,16 +50,20 @@ int macro(std::string input_avg, std::string tag,
   }
   xjjroot::setthgrstyle(hrelerrtotal, kBlack, 21, 1.0, kBlack);
   TGraphErrors* gsyst = xjjana::shifthistcenter(hsyst, "gsyst", 0);
-  xjjroot::setthgrstyle(gsyst, kGray+3, 21, 0.8, 1, 1, 1, kGray+3, 0.3, 1001, 1, 1);
-  xjjroot::setthgrstyle(hsym, kGray+3, 21, 0.8, 1, 1, 1, kGray+3, 0.3, 1001, 1, 1);
+  xjjroot::setthgrstyle(gsyst, kGray+3, 21, 0.8, 0, 0, 0, kGray+3, 0.3, 1001, 1, 1);
+  xjjroot::setthgrstyle(hsym, kGray+3, 21, 0.8, 0, 0, 0, kGray+3, 0.3, 1001, 1, 1);
 
+  auto legDATA = new TLegend(0.55, 0.45-0.030*1, 0.55+0.2, 0.45);
+  xjjroot::setleg(legDATA, 0.028);
+  legDATA->AddEntry(gsyst, "averaged & symmetrised", "pf");
+  
   auto hempty = makehempty(hsym, ";#it{#eta};d#it{N}_{ch}/d#kern[-0.08]{#it{#eta}}", 1.7);
   hempty->SetAxisRange(-3.2, 3.4, "X");  
   auto hemptyrelerr = makehempty(hrelerrtotal, ";#it{#eta};relative error");
   hemptyrelerr->SetAxisRange(-3.2, 3.4, "X");
   
   float xleg = 0.42, yleg = 0.72;
-  auto legERR = new TLegend(xleg, yleg-0.031*iavg.n(), xleg+0.2, yleg);
+  auto legERR = new TLegend(xleg, yleg-0.030*iavg.n(), xleg+0.2, yleg);
   xjjroot::setleg(legERR, 0.028);
   for(int j=0; j<iavg.n(); j++)
     legERR->AddEntry(hrelerr[j], iavg.value[j][1].c_str(), "p");
@@ -84,7 +88,7 @@ int macro(std::string input_avg, std::string tag,
   legERR->Draw();
   pdf.write("figs/comp/syst-"+tag+"-hrelerrtotal.pdf");
   
-  // // havg
+  // havg
   pdf.prepare();
   hempty->Draw("axis");
   for(auto& hh : gh1WGhadron)
@@ -96,8 +100,22 @@ int macro(std::string input_avg, std::string tag,
   // gsyst->Draw("Y0pX same");
   legPIX->Draw();
   legTRUTH->Draw();
+  legDATA->Draw();
   DRAWTEX;
   pdf.write("figs/comp/syst-"+tag+"-hsyst.pdf");
+
+  // havg
+  pdf.prepare();
+  hempty->Draw("axis");
+  for(auto& hh : gh1WGhadron)
+    hh->Draw("c same");
+  gsyst->Draw("2 same");
+  hsym->Draw("p same");
+  // gsyst->Draw("Y0pX same");
+  legTRUTH->Draw();
+  legDATA->Draw();
+  DRAWTEX;
+  pdf.write("figs/comp/syst-"+tag+"-hclean.pdf");
 
   pdf.close();
 
