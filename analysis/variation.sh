d33f11edf591e5e2cf1b875c33d5b79ad6068d8d
boundino <boundino@gmail.com>
2023-07-11 15:47:14 +0200

    add handle change for variation

--- analysis/variation.sh
+++ analysis/variation.sh
@@ -2,19 +2,22 @@
 
 asels=(
     "(1),"
-    "(vz[1]<-10&&vz[1]>-15),vz0"
-    "(vz[1]<-5&&vz[1]>-10),vz1"
-    "(vz[1]<-0&&vz[1]>-5),vz2"
-    "(vz[1]>0&&vz[1]<5),vz3"
-    "(vz[1]>5&&vz[1]<10),vz4"
-    "(vz[1]>10&&vz[1]<15),vz5"
+    # "(vz[1]<-10&&vz[1]>-15),vz0"
+    # "(vz[1]<-5&&vz[1]>-10),vz1"
+    # "(vz[1]<0&&vz[1]>-5),vz2"
+    # "(vz[1]>0&&vz[1]<5),vz3"
+    # "(vz[1]>5&&vz[1]<10),vz4"
+    # "(vz[1]>10&&vz[1]<15),vz5"
 )
 maxdr2s=(
     "0.25 drlt0p5"
-    "0.16 drlt0p4"
-    "0.36 drlt0p6"
+    # "0.16 drlt0p4"
+    # "0.36 drlt0p6"
 )
-multhandle=0 
+multhandles=(
+    "0,"
+    "1,nhit2"
+)
 ctable=0
 
 for vasels in "${asels[@]}" ; do
@@ -28,13 +31,29 @@ for vasels in "${asels[@]}" ; do
         vmaxdr2=($maxdr2)
         [[ ${vmaxdr2[0]} == 0.25 ]] && defdr2=1 || defdr2=0
 
-        [[ $(($defasel+$defdr2)) -eq 1 ]] || continue
-        
-        tagver="v1"
-        [[ $defasel -eq 1 ]] || tagver=$tagver"-"$tagasel    
-        [[ $defdr2 -eq 1 ]] || tagver=$tagver"-"${vmaxdr2[1]}
+        for mults in "${multhandles[@]}" ; do
 
-        [[ $defasel -eq 1 ]] && ./rrvar.sh 0001 10 $tagver "$asel" $maxdr2 $multhandle $ctable
-        [[ $defdr2 -eq 1 ]] && ./rrvar.sh 0001 10 $tagver "$asel" $maxdr2 $multhandle $ctable
+            IFS=',' ; vmult=($mults) ; unset IFS ;
+            multhandle=${vmult[0]}
+            tagmult=${vmult[1]}
+            [[ x$tagmult == x ]] && defmult=1 || defmult=0
+
+            # echo $multhandle
+            # echo $tagmult
+            # echo $defmult
+            
+            [[ $(($defasel+$defdr2+$defmult)) -eq 2 ]] || continue
+            
+            tagver="v1"
+            [[ $defasel -eq 1 ]] || tagver=$tagver"-"$tagasel    
+            [[ $defdr2 -eq 1 ]] || tagver=$tagver"-"${vmaxdr2[1]}
+            [[ $defmult -eq 1 ]] || tagver=$tagver"-"$tagmult    
+
+            echo $tagver
+
+            [[ $defasel -eq 0 ]] && ./rrvar.sh ${1:-0011} 00 $tagver "$asel" $maxdr2 $multhandle $ctable
+            [[ $defdr2 -eq 0 ]] && ./rrvar.sh ${1:-1011} 10 $tagver "$asel" $maxdr2 $multhandle $ctable
+            [[ $defmult -eq 0 ]] && ./rrvar.sh ${1:-1111} 11 $tagver "$asel" $maxdr2 $multhandle $ctable
+        done        
     done
 done
